fb9beca2b1a2081ba4971fda49fe4bee5a695c99
akshadabhilore-stack <akshadabhilore@gmail.com>
2026-07-18 21:50:38 +0530

    Create character.c

---
+++ character.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() 
+{
+    char ch;
+
+    printf("Enter a character: ");
+    scanf("%c", &ch);
+
+    if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z'))
+        printf("%c is an Alphabet.", ch);
+    else if (ch >= '0' && ch <= '9')
+        printf("%c is a Digit.", ch);
+    else
+        printf("%c is a Special Symbol.", ch);
+
+    return 0;
+}
